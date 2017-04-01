24d13323b904f64faa51bd9149b57e94d7c70e81
Kuma <Kuma@Macs-MacBook-Pro.local>
2017-04-01 13:57:19 -0500

    finished course

--- app/src/main/java/com/kuma/news/MainActivity.java
+++ app/src/main/java/com/kuma/news/MainActivity.java
@@ -18,43 +18,45 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding mBinding;
+  ActivityMainBinding mBinding;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        mBinding.activityMainRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    mBinding.activityMainRecyclerview.setLayoutManager(new LinearLayoutManager(this));
 
-        NewsAPI.getApi().getArticles("espn", "top").enqueue(new Callback<GetArticlesResponse>() {
-            @Override public void onResponse(Call<GetArticlesResponse> call,
-                                             Response<GetArticlesResponse> response) {
-                handleResponse(response);
-                showNewApiSnack();
-            }
+    NewsAPI.getApi().getArticles("espn", "top").enqueue(new Callback<GetArticlesResponse>() {
+      @Override public void onResponse(Call<GetArticlesResponse> call,
+                                       Response<GetArticlesResponse> response) {
+        mBinding.activityMainProgressbar.setVisibility(View.GONE);
+        handleResponse(response);
+        showNewApiSnack();
+      }
 
-            @Override public void onFailure(Call<GetArticlesResponse> call, Throwable t) {
-            }
-        });
-    }
+      @Override public void onFailure(Call<GetArticlesResponse> call, Throwable t) {
+        mBinding.activityMainProgressbar.setVisibility(View.GONE);
+      }
+    });
+  }
 
-    private void handleResponse(Response<GetArticlesResponse> response) {
-        GetArticlesResponse getArticlesResponse = response.body();
-        NewsStore.setArticles(getArticlesResponse.getArticles());
-        HomeNewsAdapter homeNewsAdapter = new HomeNewsAdapter(NewsStore.getArticles());
-        mBinding.activityMainRecyclerview.setAdapter(homeNewsAdapter);
-    }
+  private void handleResponse(Response<GetArticlesResponse> response) {
+    GetArticlesResponse getArticlesResponse = response.body();
+    NewsStore.setArticles(getArticlesResponse.getArticles());
+    HomeNewsAdapter homeNewsAdapter = new HomeNewsAdapter(NewsStore.getArticles());
+    mBinding.activityMainRecyclerview.setAdapter(homeNewsAdapter);
+  }
 
-    private void showNewApiSnack() {
-        Snackbar.make(mBinding.activityMain, "Powered by NewsApi.org", Snackbar.LENGTH_SHORT)
-                .setAction("Visit", new View.OnClickListener() {
-                    @Override public void onClick(View v) {
-                        loadNewsApiWebsite();
-                    }
-                }).show();
-    }
+  private void showNewApiSnack() {
+    Snackbar.make(mBinding.activityMain, "Powered by NewsApi.org", Snackbar.LENGTH_SHORT)
+        .setAction("Visit", new View.OnClickListener() {
+          @Override public void onClick(View v) {
+            loadNewsApiWebsite();
+          }
+        }).show();
+  }
 
-    private void loadNewsApiWebsite() {
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://newsapi.org")));
-    }
+  private void loadNewsApiWebsite() {
+    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://newsapi.org")));
+  }
 
 }
--- app/src/test/java/com/kuma/news/utils/DateUtilsTest.java
+++ app/src/test/java/com/kuma/news/utils/DateUtilsTest.java
@@ -1,26 +1,47 @@
 package com.kuma.news.utils;
 
-import org.junit.Test;
+import com.google.firebase.crash.FirebaseCrash;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.text.ParseException;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Jorge.Enciso on 31/03/2017.
  */
-public class DateUtilsTest {
-    String correctInputDate1 = "2016-07-25T09:56:27Z";
-    String correctOutputDate1 = "lun, 25 jul 2016 09:56";
+@RunWith(PowerMockRunner.class) @PrepareForTest({FirebaseCrash.class}) public class DateUtilsTest {
+  String correctInputDate1 = "2016-07-25T09:56:27Z";
+  String correctOutputDate1 = "Mon, 25 Jul 2016 09:56";
 
-    @Test
-    public void formatNewsApiDate_correctDate_outputsCorrectDate() {
-        String outputDate = DateUtils.formatNewsApiDate(correctInputDate1);
-        assertEquals(outputDate, correctOutputDate1);
-    }
+  String incorrectInputDate = "2016-07-25T09";
 
-    @Test
-    public void formatNewsApitDate_nullInput_outputsNull() {
-        String outputDate = DateUtils.formatNewsApiDate(null);
-        assertEquals(outputDate, null);
-    }
+  @Test
+  public void formatNewsApiDate_correctDate_outputsCorrectDate() {
+    String outputDate = DateUtils.formatNewsApiDate(correctInputDate1);
+    assertEquals(outputDate, correctOutputDate1);
+  }
+
+  @Test
+  public void formatNewsApitDate_nullInput_outputsNull() {
+    String outputDate = DateUtils.formatNewsApiDate(null);
+    assertEquals(outputDate, null);
+  }
+
+  @Test
+  public void formatNewsApiDate_incorrectInput_returnsSame(){
+    PowerMockito.mockStatic(FirebaseCrash.class);
+    String outputDate = DateUtils.formatNewsApiDate(incorrectInputDate);
+    assertEquals(outputDate, incorrectInputDate);
+
+    PowerMockito.verifyStatic();
+    FirebaseCrash.report(Matchers.isA(ParseException.class ));
+  }
 
 }
